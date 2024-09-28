2582bda3ed40796ab117c05ca8a446f06d2a2536
Ios group flavor fusion <lasithud@gmail.com>
2024-09-28 14:00:58 +0530

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlavorFusion
+//
+//  Created by COBSCCOMPY4231P-15 on 2024-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlavorFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorFusionApp.swift
+//  FlavorFusion
+//
+//  Created by COBSCCOMPY4231P-15 on 2024-09-28.
+//
+
+import SwiftUI
+
+@main
+struct FlavorFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+         ContentView()
+        }
+    }
+}
